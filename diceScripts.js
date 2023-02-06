a1e54e97a6e484f9145c539ebf636f6ed3067485
soren-t22 <sorencthrawl@lewisu.edu>
2023-02-05 21:27:10 -0600

    idea

--- diceScripts.js
+++ diceScripts.js
@@ -1,11 +1,11 @@
 
 function RollSixSidedDice(){
     
-    var randomnum1 = Math.floor(Math.random() * 6) + 1;
-    var randomnum2 = Math.floor(Math.random() * 6) + 1;
-    var randomnum3 = Math.floor(Math.random() * 6) + 1;
-    var randomnum4 = Math.floor(Math.random() * 6) + 1;
-    var randomnum5 = Math.floor(Math.random() * 6) + 1;
+    var randomnum1 = Math.floor(Math.random() * 6) + 1 * 10;
+    var randomnum2 = Math.floor(Math.random() * 6) + 1 * 100;
+    var randomnum3 = Math.floor(Math.random() * 6) + 1 * 1000;
+    var randomnum4 = Math.floor(Math.random() * 6) + 1 * 10000;
+    var randomnum5 = Math.floor(Math.random() * 6) + 1 * 100000;
 
     document.getElementById("dice").value = randomnum1 + randomnum2 + randomnum3 + randomnum4 + randomnum5;
     

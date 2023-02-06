b3c34853ed0081c8ad17d24c9d84d32f0b9c9588
soren-t22 <sorencthrawl@lewisu.edu>
2023-02-05 21:33:52 -0600

    die fixes

--- diceScripts.js
+++ diceScripts.js
@@ -2,11 +2,15 @@
 function RollSixSidedDice(){
     
     var randomnum1 = (Math.floor(Math.random() * 6) + 1);
-    var randomnum2 = (Math.floor(Math.random() * 6) + 1) * 10;
-    var randomnum3 = (Math.floor(Math.random() * 6) + 1) * 100;
-    var randomnum4 = (Math.floor(Math.random() * 6) + 1) * 1000;
-    var randomnum5 = (Math.floor(Math.random() * 6) + 1) * 10000;
+    var randomnum2 = (Math.floor(Math.random() * 6) + 1);
+    var randomnum3 = (Math.floor(Math.random() * 6) + 1);
+    var randomnum4 = (Math.floor(Math.random() * 6) + 1);
+    var randomnum5 = (Math.floor(Math.random() * 6) + 1);
 
-    document.getElementById("dice").value = randomnum1 + randomnum2 + randomnum3 + randomnum4 + randomnum5;
+    document.getElementById("dice1").value = randomnum1;
+    document.getElementById("dice2").value = randomnum2;
+    document.getElementById("dice3").value = randomnum3;
+    document.getElementById("dice4").value = randomnum4;
+    document.getElementById("dice5").value = randomnum5;
     
 }

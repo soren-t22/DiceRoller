597557d4fb6f031dbe393f3ffa2e69326715049b
soren-t22 <sorencthrawl@lewisu.edu>
2023-02-05 21:25:56 -0600

    hope

--- diceScripts.js
+++ diceScripts.js
@@ -1,15 +1,12 @@
 
 function RollSixSidedDice(){
-    document.getElementById("dice").value = Math.floor(Math.random()*6) + 1;
-    document.getElementById("dice").value = Math.floor(Math.random()*6) + 1;
-    document.getElementById("dice").value = Math.floor(Math.random()*6) + 1;
-    document.getElementById("dice").value = Math.floor(Math.random()*6) + 1;
-    document.getElementById("dice").value = Math.floor(Math.random()*6) + 1;
-    //var randomnum1 = Math.floor(Math.random() * 6) + 1;
-    //var randomnum2 = Math.floor(Math.random() * 6) + 1;
-    //var randomnum3 = Math.floor(Math.random() * 6) + 1;
-    //var randomnum4 = Math.floor(Math.random() * 6) + 1;
-    //var randomnum5 = Math.floor(Math.random() * 6) + 1;
+    
+    var randomnum1 = Math.floor(Math.random() * 6) + 1;
+    var randomnum2 = Math.floor(Math.random() * 6) + 1;
+    var randomnum3 = Math.floor(Math.random() * 6) + 1;
+    var randomnum4 = Math.floor(Math.random() * 6) + 1;
+    var randomnum5 = Math.floor(Math.random() * 6) + 1;
 
-    //return randomnum1, randomnum2, randomnum3, randomnum4, randomnum5
+    document.getElementById("dice").value = randomnum1 + randomnum2 + randomnum3 + randomnum4 + randomnum5;
+    
 }

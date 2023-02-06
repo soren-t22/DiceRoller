2e02e85ebf81a86f82e17c511197daa7049bd3a3
soren-t22 <sorencthrawl@lewisu.edu>
2023-02-05 20:00:48 -0600

    attempt 2

--- diceScripts.js
+++ diceScripts.js
@@ -1,8 +1,10 @@
 
-function onclick(){
+function clickevent(){
     var randomnum1 = Math.floor(Math.random() * 6) + 1;
     var randomnum2 = Math.floor(Math.random() * 6) + 1;
     var randomnum3 = Math.floor(Math.random() * 6) + 1;
     var randomnum4 = Math.floor(Math.random() * 6) + 1;
     var randomnum5 = Math.floor(Math.random() * 6) + 1;
+
+    return randomnum1, randomnum2, randomnum3, randomnum4, randomnum5
 }
